9ee5599923b83d23fd251656b6d2f32cae2f4957
Dylan Ngo <dylanngo>
2023-12-31 19:36:42 +0800

    add doctrine

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,12 +5,12 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class Lucky
+class LuckyController
 {
 	/**
 	 * @throws \Exception
 	 */
-	#[Route('/lucky')]
+	#[Route('/lucky', name: 'app_lucky')]
 	public function number(): Response
 	{
 		$number = random_int(0, 100);
